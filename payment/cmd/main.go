0aa03b13a1ec08aa83e03b00522ce4fc6716fb1d
Sergey P (home) <pinai4@gmail.com>
2025-10-05 13:08:05 +0300

    Added /payment module. Generated proto code for /payment module in /shared module

---
+++ payment/cmd/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/google/uuid"
+	paymentV1 "github.com/pinai4/microservices-course-project/shared/pkg/proto/payment/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const grpcPort = 50051
+
+type paymentService struct {
+	paymentV1.UnimplementedPaymentServiceServer
+}
+
+func (s *paymentService) PayOrder(ctx context.Context, request *paymentV1.PayOrderRequest) (*paymentV1.PayOrderResponse, error) {
+	tranID := uuid.New()
+	return &paymentV1.PayOrderResponse{TransactionUuid: tranID.String()}, nil
+}
+
+func PrinterInterceptor() grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		resp, err := handler(ctx, req)
+		if err == nil {
+			if r, ok := resp.(*paymentV1.PayOrderResponse); ok && r != nil {
+				log.Printf("Payment was successful, %s\n", r.GetTransactionUuid())
+			}
+		}
+
+		return resp, err
+	}
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Printf("failed to listen: %v\n", err)
+		return
+	}
+
+	// Create GRPC server
+	s := grpc.NewServer(grpc.ChainUnaryInterceptor(
+		PrinterInterceptor(),
+	))
+
+	// Register our service
+	service := &paymentService{}
+
+	paymentV1.RegisterPaymentServiceServer(s, service)
+
+	// Enable GRPC reflection to simplify debugging
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("🚀 gRPC server listening on %d\n", grpcPort)
+		if err := s.Serve(lis); err != nil {
+			log.Printf("failed to serve: %v\n", err)
+			return
+		}
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("🛑 Shutting down gRPC server...")
+	s.GracefulStop()
+	log.Println("✅ Server stopped")
+}
